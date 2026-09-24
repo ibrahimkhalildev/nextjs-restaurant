fbdf2f317d1cb31b69cfea595d5ee1a5ee2c9540
ibrahimkhalildev <ibrahimkholil01@gmail.com>
2026-09-24 06:01:13 +0600

    use client

--- src/app/components/Counter.jsx
+++ src/app/components/Counter.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+"use client"
+
+import React, { useState } from 'react'
 
 const Counter = () => {
-    return (
-        <div>
-            <h2 className='text-4xl font-bold mb-4'>Counter</h2>
-        </div>
-    );
-};
+  const [count, setCount] = useState(0)
+  const handleIncrease = () => {
+    console.log('Increase button clicked')
+    setCount(count + 1)
+  }
 
-export default Counter;
+  console.log('Counter component Render')
+  return (
+    <div>
+      <h2 className='text-4xl font-bold mb-4'>Counter: {count}</h2>
+
+      <button
+        onClick={handleIncrease}
+        class='bg-sky-500 hover:bg-amber-200 p-2 rounded-2xl'
+      >
+        Increase
+      </button>
+    </div>
+  )
+}
+
+export default Counter
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from 'react'
+import Counter from '../components/Counter'
 
 const DashboardPage = () => {
-    return (
-        <div>
-            <h2>Dashboard Page</h2>
-            <ul>
-                <li>Item 01</li>
-                <li>Item 02</li>
-                <li>Item 03</li>
-                <li>Item 04</li>
-                <li>Item 05</li>
-            </ul>
-        </div>
-    );
-};
+  console.log('Server page rendered!')
+  return (
+    <div>
+      <h2>Dashboard Page</h2>
+      <Counter></Counter>
+      <ul>
+        <li>Item 01</li>
+        <li>Item 02</li>
+        <li>Item 03</li>
+        <li>Item 04</li>
+        <li>Item 05</li>
+      </ul>
+    </div>
+  )
+}
 
-export default DashboardPage;
-
+export default DashboardPage
